dd5397ed8976ec37b2a9f07ed18d5e615082e436
pandax1234 <1014620202@unajma.edu.pe>
2024-12-17 23:01:58 -0500

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyB2iYgmbSGsbz17bn6GQ--EkvYxMPDS0ns",
+    authDomain: "redsocial2-43162.firebaseapp.com",
+    projectId: "redsocial2-43162",
+    storageBucket: "redsocial2-43162.firebasestorage.app",
+    messagingSenderId: "562172392344",
+    appId: "1:562172392344:web:7746a4c1ac998a4f7c31ed"
+  },
+};
+
